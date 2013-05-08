daabf37bab997d81b237aa70cd00598c5c01d0bd
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-05-08 14:01:40 -0400

    Add session 5 materials.

--- 5/lightsensor_to_led/lightsensor_to_led.ino
+++ 5/lightsensor_to_led/lightsensor_to_led.ino
@@ -1,5 +1,9 @@
+/* Code to use a light sensor to control the brightness of an LED. This is
+ * identical to the code for using an analog force sensor except that the range
+ * of input values is different.
+ */
 int LED_PIN = 3;
-int forceSensorPin = A0;
+int lightSensorPin = A0;
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
@@ -7,17 +11,17 @@ void setup() {
 }
 
 void loop() {
-    // Read the force value from the analog input sensor
-    int force = analogRead(forceSensorPin);
-    Serial.print("Actual force: ");
-    Serial.print(force);
+    // Read the light value from the analog input sensor
+    int light = analogRead(lightSensorPin);
+    Serial.print("Actual light level: ");
+    Serial.print(light);
 
-    // The input value for force is between 0 and 1023, but depending on the
+    // The input value for light is between 0 and 1023, but depending on the
     // size of your foam it will probably stay between 400 and 600. To control
     // the output of the LED, we need a number between 0 and 255.
     //
-    // Map the input values (400 to 600) to an output value (0 to 255)
-    int ledValue = map(force, 440, 600, 0, 255);
+    // Map the input values to an output value (0 to 255)
+    int ledValue = map(light, 300, 680, 0, 255);
     // The "map" function will return a negative value if we go outside of the
     // 440-600 range, and we don't want thing. This will make sure the output
     // value is always positive
